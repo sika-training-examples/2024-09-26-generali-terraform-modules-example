66ebd54562d569f982806c96261532f65970a50e
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-26 15:50:40 +0200

    feat(storage): Add resource group to storage module

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,3 +1,11 @@
+resource "azurerm_resource_group" "this" {
+  lifecycle { ignore_changes = [tags] }
+
+  name     = "rg-${var.name}-storage"
+  location = var.location
+  tags     = var.tags_common
+}
+
 resource "azurerm_storage_account" "this" {
   lifecycle {
     ignore_changes = [
@@ -5,8 +13,8 @@ resource "azurerm_storage_account" "this" {
     ]
   }
   name                     = var.name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags                     = var.tags_common
